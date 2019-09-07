308f36c27dcc3cb0277b4752f63bd3d532f81f86
Yu Ishiguro <yuuis.egr@gmail.com>
2019-09-07 16:14:36 +0900

    add ishi method

--- app/controllers/tapis_controller.rb
+++ app/controllers/tapis_controller.rb
@@ -5,7 +5,7 @@ class TapisController < ApplicationController
     return render json: { message: 'image param is required' }, status: :bad_request if params[:image].nil?
 
     photo = params[:image]
-    @result = yama(photo)
+    @result = ishi(yama(photo))
 
     return render json: { message: 'your uploaded image is not tapioka!!!!' }, status: :bad_request if @result.nil?
 
@@ -44,5 +44,39 @@ class TapisController < ApplicationController
     response = https.request(request, body)
 
     JSON.parse(response.body)
+  end
+
+  def ishi(result)
+    labels = JSON.parse(result)['labelAnnotations']
+
+    @yasashi = 0.2
+    @reisei = 0.2
+    @uchiki = 0.2
+    @narushisuto = 0.2
+    @genki = 0.2
+
+    labels.each do |label|
+      @yasashi += label['score'].to_f if label['description'].include?("Milk")
+      @reisei += label['score'].to_f if label['description'].include?("Coffee")
+      @uchiki += label['score'].to_f if label['description'].include?("Cup")
+      @narushisuto += label['score'].to_f if label['description'].include?("Drink")
+      @genki += label['score'].to_f if label['description'].include?("FOOD")
+    end
+
+    total = @yasashi + @reisei + @uchiki + @narushisuto + @genki
+
+    yasashi_per = (@yasashi / total).to_f
+    reisei_per = (@reisei / total).to_f
+    uchiki_per = @uchiki / total
+    narushisuto_per = @narushisuto / total
+    genki_per = @genki / total
+
+    {
+      :yasashi => yasashi_per,
+      :reisei => reisei_per,
+      :uchiki => uchiki_per,
+      :narushisuto => narushisuto_per,
+      :genki => genki_per
+    }
   end
 end
