35ec6726969b2dc631dd5ec8c791760ef2bc3336
Yu Ishiguro <yuuis.egr@gmail.com>
2019-09-07 14:19:27 +0900

    作ったぞ！！

--- app/controllers/tapis_controller.rb
+++ app/controllers/tapis_controller.rb
@@ -1,4 +1,12 @@
 class TapisController < ApplicationController
   def create
+    return render json: { message: "image param is required" }, status: :bad_request if params[:image].empty?
+
+    photo = params[:image]
+    @result = yama(photo)
+
+    return render json: { message: "your uploaded image is not tpaioka!!!!" }, status: :bad_request if @result.nil?
+
+    render json: @result, status: :ok
   end
 end
